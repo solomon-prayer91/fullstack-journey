159da4da9101d0e124860642e12509a542290ba8
Solomon-prayer91 <soloventures01@gmail.com>
2026-09-09 09:02:30 +0100

    Day 8 - Form validation working + pro whit headshot

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ function toggleTheme() {
     // Add or remove dark class
 body.classList.toggle("dark-mode");
 }
+
+function sendMessage() {
+    //Get values from the inputs
+    let name = document.getElementById("visitorName").value.trim();
+    let email = document.getElementById("visitorEmail").value.trim();
+    let message = document.getElementById("visitorMessage").value.trim();
+    let status = document.getElementById("formStatus");
+
+    // Validation - check if empty
+    if (name === " " || email === " " || message === " ") {
+        status.textContent = "Please fill in all fields!";
+        status.style.color = "red";
+        return; //stop here
+    }
+
+    // Simple email check - must contain @
+    if (!email.includes("@")) {
+        status.textContent = "Please enter a valid email with @";
+        status.style.color = "red";
+        return; // stop here
+    }
+
+    // If we reach here, everything is good
+    status.textContent = "Thanks " + name + "! Message sent (demo). I will reply at " + email;
+    status.style.color = "green";
+
+    // Clear inputs
+    document.getElementById("visitorName").value = " ";
+    document.getElementById("visitorEmail").value = " ";
+    document.getElementById("visitorMessage").value = " ";
+
+}
